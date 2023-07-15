ab09059a4c1701046be14e498daa406ffa2d3033
Karan <karangrewal979@gmail.com>
2023-07-15 16:27:09 +0530

    Add a random number generator from 1 to 3

---
+++ game.js
@@ -0,0 +1,20 @@
+//get computer's choice for rock paper and scissors
+//get a random number from the computer ranging from 1 to 3
+//assign rock paper and scissors to above random numbers
+function getComputerChoice(){
+    Math.floor(Math.random()*3)+ 1;
+    
+}
+//get user's choice 
+function getUserChoice(){
+
+}
+//Compare both choices and display who wins
+function roundResult(){
+
+}
+//repeat the above process 5 times 
+function game(){
+
+}
+//display the final result
