f38175acbd4e78f2b537d0ad12c3ea1f260691e4
Karan <karangrewal979@gmail.com>
2023-07-16 02:55:36 +0530

    Changes while printing console
    -removed assign the value of computerChoice function to const computerSelection parameter
    -removed fiveTimes function, instead used console.log five times

--- game.js
+++ game.js
@@ -21,68 +21,58 @@ it has two parameters-playerSelection and computerSelection*/
 function gameRound(playerSelection ,computerSelection){
 /*if playerSelection is equals to rock and computerSelection
  is equal to rock(lower case)*/
-if(playerSelection == "rock"&&computerSelection.toLowerCase() =="rock"){
+if(playerSelection == "rock"&&computerSelection =="rock"){
  //return it's a tie b/w rocks !
 return("It's a tie between Rocks");}
  /*if playerSelection is equals to paper and computerSelection
  is equal to rock(lower case)*/ 
- if(playerSelection == "paper"&&computerSelection.toLowerCase() =="rock"){
+ if(playerSelection == "paper"&&computerSelection =="rock"){
  //return you won! paper beats rock
 return("You won! Paper beats Rock");}
  /*if playerSelection is equals to scissor and computerSelection
  is equal to rock(lower case)*/ 
- if(playerSelection == "scissor"&&computerSelection.toLowerCase() =="rock"){
+ if(playerSelection == "scissor"&&computerSelection =="rock"){
  //return you lose ! scissor loses to rock
 return("You lose! Scissor loses to Rock");}
  /*if playerSelection is equals to rock and computerSelection
  is equal to paper(lower case)*/
- if(playerSelection == "rock"&&computerSelection.toLowerCase() =="paper"){
+ if(playerSelection == "rock"&&computerSelection =="paper"){
  //return you lose! rock loses to paper
 return("You lose! Rock loses to Paper");}
  /*if playerSelection is equals to rock and computerSelection
  is equal to scissor(lower case)*/ 
- if(playerSelection == "rock"&&computerSelection.toLowerCase() =="scissor"){
+ if(playerSelection == "rock"&&computerSelection =="scissor"){
  //return you won! rock beats scissors
 return("You won! Rock beats Scissor");}
 /*if playerSelection is equals to paper and computerSelection
  is equal to paper(lower case)*/ 
- if(playerSelection == "paper"&&computerSelection.toLowerCase() =="paper"){
+ if(playerSelection == "paper"&&computerSelection =="paper"){
  //return it's a tie b/w papers!
 return("It's a tie between Papers");}
  /*if playerSelection is equals to scissors and computerSelection
  is equal to scissors(lower case)*/ 
- if(playerSelection == "scissor"&&computerSelection.toLowerCase() =="scissor"){
+ if(playerSelection == "scissor"&&computerSelection == "scissor"){
  //return it's a tie b/w scissors
 return("It's a tie between scissors");}
  /*if playerSelection is equals to paper and computerSelection
  is equal to scissor(lower case)*/ 
- if(playerSelection == "paper"&&computerSelection.toLowerCase() =="scissor"){
+ if(playerSelection == "paper"&&computerSelection =="scissor"){
  //return you lose! paper loses to scissor
 return("You lose! Paper loses to scissor");}
  /*if playerSelection is equals to scissors and computerSelection
  is equal to paper(lower case)*/ 
- if(playerSelection == "scissor"&&computerSelection.toLowerCase() =="paper"){
+ if(playerSelection == "scissor"&&computerSelection =="paper"){
  //return you won! scissor beats paper
 return("You won! Scissor beats Paper");}
  }
- //assign the value of computerChoice function to const computerSelection parameter
-const computerSelection = computerChoice();
- //assign the value of playerChoice to const playerSelection parameter
+ //assign the value of playerChoice 
 let playerChoice  = prompt("Please select - Rock , Paper or Scissor");
-const playerSelection  = playerChoice;
- /*create a function named fiveTimes having parameters - rockPaperScissor and times*/
-function fiveTimes(rockPaperScissor,times){
- //if times > 0
-if(times = 0){
- //return rockPaperScissor.repeat(times) to repeat the game multiple times
-return(rockPaperScissor.repeat(times));}
- //else return ""
-else{return("")} 
-}
+
  /*print the above function using console.log and initialize the arguments
   in the parameter of the above function*/
-console.log(fiveTimes(gameRound(playerSelection,computerChoice),5));
+console.log(gameRound(playerChoice,computerChoice()));
 
 
 
 
+//assign the value of computerChoice function to const computerSelection parameter
