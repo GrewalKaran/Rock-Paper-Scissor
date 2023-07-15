6e76526f0ffc9142b2e4f2419bf991a93ed3fa0d
Karan <karangrewal979@gmail.com>
2023-07-15 16:47:28 +0530

    Assign random numbers to rock.paper.scissors

--- game.js
+++ game.js
@@ -2,9 +2,16 @@
 //get a random number from the computer ranging from 1 to 3
 //assign rock paper and scissors to above random numbers
 function getComputerChoice(){
-    Math.floor(Math.random()*3)+ 1;
-    
-}
+    let random = Math.floor(Math.random()*3)+ 1;
+    if(random == 1){          /*dont use = because it will assign the value to random variable, 
+                               instead == will compare the random variable */  
+        console.log("rock");
+    }else if(random == 2){
+        console.log("paper");
+    }else if(random == 3){
+        console.log("scissors");
+    }
+}getComputerChoice();
 //get user's choice 
 function getUserChoice(){
 
